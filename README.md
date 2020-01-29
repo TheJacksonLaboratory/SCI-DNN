8e24f23211115b2b8f4d92302e3e8e790c858f00
Haitham Ashoor <haitham.ashoor@kaust.edu.sa>
2020-01-29 13:38:13 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# SCI-DNN
+**Sub-compartment classifier**
+--------------
+
+**Authors**: Haitham Ashoor, Sheng Li  **Contact**: haitham.ashoor@jax.org, sheng.li@jax.org
+
+
+## Description
+This is model for classifier described in our paper "Graph embedding resolves genomic sub-compartments
+ and powers deep neural network predictive model"
+ 
+ 
+ # Dependencies
+ * Python >= 3.5
+ * Keras 2.2.4
+ * scikit-learn == 0.19.0
+ * Numpy >= 1.15
+ 
+ 
+## Example:
+```sh
+$ python run_subcompartment_classifier.py -x testing_data.txt -y testing_labels.txt -s sub_compartments_model.json -w 
+sub_compartments_model.h5 -o predicted_labes.txt
+```
+
+ 
---
+++ run_subcompartment_classifier.py
@@ -0,0 +1,88 @@
+import keras
+import numpy as np
+from sklearn.metrics import accuracy_score
+import argparse
+
+
+def load_keras_model(model_json,model_h5):
+    #load model structure
+    json_file = open (model_json, 'r')
+    loaded_model_json = json_file.read ()
+    json_file.close ()
+    model = keras.models.model_from_json (loaded_model_json)
+
+    # load weights into new model
+    model.load_weights (model_h5)
+
+    return model
+
+
+def load_data(test_file,test_labels):
+    X_test = np.loadtxt(test_file,delimiter="\t")
+
+    if test_file != None:
+        y_test = np.loadtxt(test_labels,delimiter="\t")
+        return X_test,y_test
+    return X_test,[]
+
+
+def classify(model,X_test,predictions_file,y_test):
+
+    y_pred = model.predict_classes (X_test)
+    np.savetxt(predictions_file,y_pred)
+
+    #print(y_test)
+    if y_test != []:
+        acc = accuracy_score (y_test, y_pred)
+        print ("Model accuracy is: %f" % acc)
+
+
+
+def run_subcompartment_classifier():
+    parser = argparse.ArgumentParser(description=("Sub-compartment classifier based on epigenomic and sequence data"))
+    parser.add_argument("-x",
+                        "--test_data",
+                        action="store",
+                        dest="x_test",
+                        help="File has testing data in form feature",
+                        type=str,
+                        required=True)
+
+    parser.add_argument ("-y",
+                         "--test_labels",
+                         action="store",
+                         dest="y_test",
+                         help="File has testing data true predictions, for benchmarking purpose",
+                         type=str,
+                         required=False,
+                         default=None)
+    parser.add_argument ("-s",
+                         "--model_structure",
+                         action="store",
+                         dest="model_json",
+                         help="JSON file describing neural network structure",
+                         type=str,
+                         required=True)
+    parser.add_argument ("-w",
+                         "--model_weights",
+                         action="store",
+                         dest="model_weights",
+                         help="h5 file contains neural network parameters",
+                         type=str,
+                         required=True)
+    parser.add_argument ("-o",
+                         "--output_file",
+                         action="store",
+                         dest="output",
+                         help="File name where predictions will be stored",
+                         type=str,
+                         required=True)
+
+    oArgs = parser.parse_args()
+    model = load_keras_model(oArgs.model_json,oArgs.model_weights)
+    X_test,y_test = load_data(oArgs.x_test,oArgs.y_test)
+
+    classify(model,X_test,oArgs.output,y_test)
+
+if __name__ == '__main__':
+    run_subcompartment_classifier()
